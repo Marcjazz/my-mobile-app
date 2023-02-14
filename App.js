39040cbc0cb5a6d813651b2d8af020bcd967049e
Marco Kuidja <kuidjamarco@gmail.com>
2023-02-14 01:16:22 +0100

    building todo app with react-native

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {
+      todoId: 0,
+      isDone: false,
+      todo: 'Code your first react native app',
+    },
+  ])
+  const [input, setInput] = useState('')
+
+  const onPressHandler = (todoId) => {
+    setTodos(
+      ...todos.map((todo) =>
+        todo.todoId === todoId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello world !!!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to my todo app!!!</Text>
+      {todos.map(({ todoId, todo, isDone }) => (
+        <Text
+          key={todoId}
+          onPress={onPressHandler}
+          style={isDone ? styles.doneTodo : styles.todo}
+        >
+          {todo}
+        </Text>
+      ))}
+      <View style={styles.inputBox}>
+        <TextInput onChange={(input) => setInput(input)} value={input} />
+        <Button
+          title='Add'
+          onPress={() =>
+            setTodos([
+              { isDone: false, todo: input, todoId: todos.length },
+              ...todos,
+            ])
+          }
+        />
+      </View>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +56,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  inputBox: {
+    padding: 16,
+  },
+  todo: {
+    padding: 16,
+    backgroundColor: 'grey',
+  },
+  doneTodo: {
+    padding: 16,
+    backgroundColor: 'grey',
+    textDecorationLine: 'line-through',
+  },
+})
